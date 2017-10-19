b824ec98181acf7b484f0058aa896921ad954180
hellozjf <908686171@qq.com>
2017-10-19 11:06:45 +0800

    Add changeLocalDateToDate.

---
+++ src/main/java/com/hellozjf/tool4j/DateUtils.java
@@ -0,0 +1,30 @@
+package com.hellozjf.tool4j;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+/**
+ * 时间工具类
+ * 
+ * 将本地日期时间与Date类进行转化
+ * @author hellozjf
+ *
+ */
+public class DateUtils {
+
+    /**
+     * 将LocalDate转化为Date
+     * 
+     * 转化的时候时间默认为0:00:00
+     * @param localDate     日期
+     * @param hourOffset    时区
+     * @return              0时区的Date
+     */
+    public Date changeLocalDateToDate(LocalDate localDate, int hourOffset) {
+        Instant instant = localDate.atStartOfDay().toInstant(ZoneOffset.ofHours(hourOffset));
+        return Date.from(instant);
+    }
+    
+}

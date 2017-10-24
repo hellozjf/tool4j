118da23eb29f4213a2a2c75b251cc4a082b549e0
hellozjf <908686171@qq.com>
2017-10-24 15:13:09 +0800

    Add getUserDirProperty and getUserDirPropertyValue

--- src/main/java/com/hellozjf/tool4j/PropertiesUtils.java
+++ src/main/java/com/hellozjf/tool4j/PropertiesUtils.java
@@ -1,5 +1,10 @@
 package com.hellozjf.tool4j;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 /**
@@ -11,7 +16,7 @@ import java.util.ResourceBundle;
 public class PropertiesUtils {
 
     /**
-     * 从StringResource.properties获取字符串
+     * 从classpath下的StringResource.properties获取字符串
      * 根据当前的语言信息，从相应的StringResource.properties中获取相应的字符串
      * 
      * @param key
@@ -23,7 +28,7 @@ public class PropertiesUtils {
     }
 
     /**
-     * 从一个文件中获取字符串 根据当前的语言信息，从相应的filePathName对应的properties文件中获取相应的字符串
+     * 从classpath下的一个文件中获取字符串 根据当前的语言信息，从相应的filePathName对应的properties文件中获取相应的字符串
      * 
      * @param filePathName
      *            需要读取的properties文件，没有后缀.properties
@@ -33,6 +38,37 @@ public class PropertiesUtils {
      */
     public static String getPropertyValue(String filePathName, String key) {
         return ResourceBundle.getBundle(filePathName).getString(key);
+    }
+    
+    /**
+     * 获取user.dir下面的Property文件
+     * 
+     * @param folder        如果是当前目录就写.，否则写目录，用File.separator分隔目录
+     * @param fileName      文件名，没有后缀.properteis
+     * @return              Properties对象
+     * @throws IOException  读取出现异常
+     */
+    public static Properties getUserDirProperty(String folder, String fileName) throws IOException {
+        Properties properties = new Properties();
+        String userDir = System.getProperty("user.dir");
+        String file = userDir + File.separator + folder + File.separator + fileName + ".properties";
+        InputStream in = new FileInputStream(file);
+        properties.load(in);
+        return properties;
+    }
+    
+    /**
+     * 获取user.dir下面的Property文件里面的某项value
+     * 
+     * @param folder        如果是当前目录就写.，否则写目录，用File.separator分隔目录
+     * @param fileName      文件名，没有后缀.properteis
+     * @param key           需要读取的key
+     * @return              读取出来的value
+     * @throws IOException  读取出现异常
+     */
+    public static String getUserDirPropertyValue(String folder, String fileName, String key) throws IOException {
+        Properties properties = getUserDirProperty(folder, fileName);
+        return properties.getProperty(key);
     }
 
     private PropertiesUtils() {
--- src/test/java/com/hellozjf/tool4j/PropertiesUtilsTest.java
+++ src/test/java/com/hellozjf/tool4j/PropertiesUtilsTest.java
@@ -2,6 +2,8 @@ package com.hellozjf.tool4j;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Properties;
+
 import org.junit.Test;
 
 public class PropertiesUtilsTest {
@@ -16,5 +18,17 @@ public class PropertiesUtilsTest {
     public void getPropertyValueTest() {
         String value = PropertiesUtils.getPropertyValue("StringResource", "test.string");
         assertEquals(value, "teststring");
+    }
+    
+    @Test
+    public void getUserDirPropertyTest() throws Exception {
+        Properties properties = PropertiesUtils.getUserDirProperty("conf", "StringResource");
+        assertEquals("teststring", properties.getProperty("test.string"));
+    }
+    
+    @Test
+    public void getUserDirPropertyValueTest() throws Exception {
+        String value = PropertiesUtils.getUserDirPropertyValue("conf", "StringResource", "test.string");
+        assertEquals("teststring", value);
     }
 }
